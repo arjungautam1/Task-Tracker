ee206fcbe0abc3151ed6922b7cbcece1d95df323
arjun-sudo <arjungautam5431@gmail.com>
2021-03-04 14:57:11 +0545

    Refortoring to use backend

--- src/App.vue
+++ src/App.vue
@@ -55,8 +55,24 @@ export default {
       }
 
     },
-    toggleReminder(id) {
-      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+    async toggleReminder(id) {
+
+      const taskToToggle = await this.fetchTask(id)
+      const updTask = {
+        ...taskToToggle, reminder: !taskToToggle.reminder
+      }
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updTask)
+      })
+      const data=await res.json()
+
+      this.tasks = this.tasks.map((task) =>
+          task.id === id ?
+              {...task, reminder:data.reminder} : task)
     },
 
     async fetchTasks() {
